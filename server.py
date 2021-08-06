64da330df87e6bd3100dae7e4b24de17da0eaf99
Wilson Antury <ventas@antury.net>
2021-08-06 13:35:06 -0500

    [ADD] create an user and save it into DB

--- server.py
+++ server.py
@@ -41,7 +41,16 @@ class BlogPost(db.Model):
 
 @app.route("/user", methods=["POST"])
 def create_user():
-    pass
+    data = request.get_json()
+    new_user = User(
+        name=data["name"],
+        email=data["email"],
+        address=data["address"],
+        phone=data["phone"],
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message":"user created"}, 200)
 
 @app.route("/user/descending_id", methods=["GET"])
 def get_all_users_descending():
