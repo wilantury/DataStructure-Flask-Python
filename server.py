298a0b21855f9b01f6aace1974fa4554f1413fe2
Wilson Antury <ventas@antury.net>
2021-08-06 13:27:50 -0500

    [ADD] routs and start the application server

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+from os import name
 from sqlite3 import Connection as SQLite3Connection
 from datetime import datetime
 from flask import Flask, request, jsonify
@@ -37,3 +38,42 @@ class BlogPost(db.Model):
     body = db.Column(db.String(200))
     date = db.Column(db.Date())
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+@app.route("/user", methods=["POST"])
+def create_user():
+    pass
+
+@app.route("/user/descending_id", methods=["GET"])
+def get_all_users_descending():
+    pass
+
+@app.route("/user/ascending_id", methods=["GET"])
+def get_all_users_ascending():
+    pass
+
+@app.route("/user/<user_id>", methods=["GET"])
+def get_one_user(user_id):
+    pass
+
+@app.route("/user/<user_id>", methods=["DELETE"])
+def delete_one_user(user_id):
+    pass
+
+@app.route("/blog_post/<user_id>", methods=["POST"])
+def create_blog_post(user_id):
+    pass
+
+@app.route("/user/<user_id>", methods=["GET"])
+def get_all_blog_post(user_id):
+    pass
+
+@app.route("/blog_post/<blog_post_id>", methods=["GET"])
+def get_one_blog_post(blog_post_id):
+    pass
+
+@app.route("/blog_post/<blog_post_id>", methods=["DELETE"])
+def delete_one_blog_post(blog_post_id):
+    pass
+
+if __name__=="__main__":
+    app.run(debug=True)
