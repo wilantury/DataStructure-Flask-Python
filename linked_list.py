4855be69ccb0c7da49fc7f19cc8a3b82c4e45131
Wilson Antury <ventas@antury.net>
2021-08-09 18:48:10 -0500

    [ADD] to_list method into linked list class, get all users from db, insert all users into the linked list

--- linked_list.py
+++ linked_list.py
@@ -8,6 +8,16 @@ class LinkedList:
         self.head = None
         self.last_node = None
 
+    def to_list(self):
+        arr = []
+        if self.head is None:
+            return arr
+        node = self.head
+        while node:
+            arr.append(node.data)
+            node = node.next_node
+        return arr
+
     def print_ll(self):
         ll_string = ""
         node = self.head
@@ -20,30 +30,18 @@ class LinkedList:
         print(ll_string)
     
     def insert_beginning(self, data):
+        if self.head is None:
+            self.head = Node(data, None)
+            self.last_node = self.head
+            return
+
         node = Node(data, self.head)
         self.head = node
     
     def insert_at_end(self, data):
         if self.head is None:
             self.insert_beginning(data)
-        if self.last_node is None:
-            node = self.head
-            while node.next_node:
-                node = node.next_node
-            
-            node.next_node = Node(data, None)
-            self.last_node = node.next_node
-        else:
-            self.last_node.next_node = Node(data, None)
-            self.last_node = self.last_node.next_node
+            return
 
-ll = LinkedList()
-node1 = Node("dataN1")
-node2 = Node("dataN2", node1)
-node3 = Node("dataN3", node2)
-node4 = Node("dataN4", node3)
-ll.head = node4
-ll.print_ll()
-ll.insert_beginning("data_new")
-ll.print_ll()
-
+        self.last_node.next_node = Node(data, None)
+        self.last_node = self.last_node.next_node
--- server.py
+++ server.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify
 from sqlalchemy import event
 from sqlalchemy.engine import Engine
 from flask_sqlalchemy import SQLAlchemy
+import linked_list
 
 
 app = Flask(__name__)
@@ -54,7 +55,19 @@ def create_user():
 
 @app.route("/user/descending_id", methods=["GET"])
 def get_all_users_descending():
-    pass
+    users = User.query.all()
+    all_users_ll = linked_list.LinkedList()
+    for user in users:
+        all_users_ll.insert_beginning(
+            {
+                "id":user.id,
+                "name":user.name,
+                "email":user.email,
+                "address":user.address,
+                "phone":user.phone
+            }      
+        )
+    return jsonify(all_users_ll.to_list()), 200
 
 @app.route("/user/ascending_id", methods=["GET"])
 def get_all_users_ascending():
