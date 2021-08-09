641ff0f25fff6d31dd4ec8b360d70cffc847a5a5
Wilson Antury <ventas@antury.net>
2021-08-09 18:06:06 -0500

    [ADD] insert at end of linked list method

--- linked_list.py
+++ linked_list.py
@@ -26,6 +26,16 @@ class LinkedList:
     def insert_at_end(self, data):
         if self.head is None:
             self.insert_beginning(data)
+        if self.last_node is None:
+            node = self.head
+            while node.next_node:
+                node = node.next_node
+            
+            node.next_node = Node(data, None)
+            self.last_node = node.next_node
+        else:
+            self.last_node.next_node = Node(data, None)
+            self.last_node = self.last_node.next_node
 
 ll = LinkedList()
 node1 = Node("dataN1")
